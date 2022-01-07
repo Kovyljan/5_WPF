078bd9aa8222ae4adfcf0f47c1c3c203675d695e
kovyljan <kovyljan@mail.ru>
2022-01-07 23:06:10 +0400

    change_1

--- 5.2_WPF/MainWindow.xaml.cs
+++ 5.2_WPF/MainWindow.xaml.cs
@@ -70,22 +70,61 @@ namespace _5._2_WPF
 
         private void MenuItem_Click_2(object sender, RoutedEventArgs e)
         {
+            //SaveFileDialog save_file_dialog = new SaveFileDialog();
+            //save_file_dialog.Filter = "PNG (*.png)|*.png|Все файлы (*.*)|*.*";
+            //if (save_file_dialog.ShowDialog() == true)
+            //{
+            //    string file_name = save_file_dialog.FileName;
+            //    int margin = (int)this.ink_canvas.Margin.Left;
+            //    int width = (int)this.ink_canvas.ActualWidth - margin;
+            //    int height = (int)this.ink_canvas.ActualHeight - margin;
+            //    RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            //    rtb.Render(ink_canvas);
+
+            //    using (FileStream fs = new FileStream(file_name, FileMode.Create))
+            //    {
+            //        BmpBitmapEncoder bbe = new BmpBitmapEncoder();
+            //        bbe.Frames.Add(BitmapFrame.Create(rtb));
+            //        bbe.Save(fs);
+            //    }
+            //}
+
+
+            var pSource = PresentationSource.FromVisual(Application.Current.MainWindow);
+            Matrix m = pSource.CompositionTarget.TransformToDevice;
+            double dpiX = m.M11 * 96;
+            double dpiY = m.M22 * 96;
+
+            int width = (int)this.ink_canvas.ActualWidth;
+            int height = (int)this.ink_canvas.ActualHeight;
+
+            var elementBitmap = new RenderTargetBitmap(width, height, dpiX, dpiY, PixelFormats.Default);
+
+            var drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                var visualBrush = new VisualBrush(ink_canvas);
+                drawingContext.DrawRectangle(
+                    visualBrush,
+                    null,
+                    new Rect(new Point(0, 0), new Size(width / m.M11, height / m.M22)));
+            }
+
+            elementBitmap.Render(drawingVisual);
+
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(elementBitmap));
+
             SaveFileDialog save_file_dialog = new SaveFileDialog();
             save_file_dialog.Filter = "PNG (*.png)|*.png|Все файлы (*.*)|*.*";
             if (save_file_dialog.ShowDialog() == true)
             {
                 string file_name = save_file_dialog.FileName;
-                int margin = (int)this.ink_canvas.Margin.Left;
-                int width = (int)this.ink_canvas.ActualWidth - margin;
-                int height = (int)this.ink_canvas.ActualHeight - margin;
-                RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
-                rtb.Render(ink_canvas);
-
-                using (FileStream fs = new FileStream(file_name, FileMode.Create))
+                using (var imageFile = new FileStream(file_name, FileMode.Create, FileAccess.Write))
                 {
-                    BmpBitmapEncoder bbe = new BmpBitmapEncoder();
-                    bbe.Frames.Add(BitmapFrame.Create(rtb));
-                    bbe.Save(fs);
+                    encoder.Save(imageFile);
+                    imageFile.Flush();
+                    imageFile.Close();
                 }
             }
         }
